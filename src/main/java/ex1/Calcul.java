802ca1e6a56b1e47149a543caed3536d0be4bbe7
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-05-17 14:16:28 +0200

    fix: exercice 1 not comitted

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,24 @@
+package ex1;
+
+public class Calcul {
+
+	/**
+	 * Sum the 2 integer parameters and return the result
+	 * @param a Integer
+	 * @param b Integer
+	 * @return Result of the sum
+	 */
+	public int sum(int a, int b){
+		return a+b;
+	}
+	
+	/**
+	 * Substract b from a and return the result
+	 * @param a Integer
+	 * @param b Integer
+	 * @return Result of the substract
+	 */
+	public int subtract(int a, int b){
+		return a-b;
+	}
+}
